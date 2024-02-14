2f1e33ac09cc3af8d068cc9722914bb1dd496c56
Biel Pujadas Suriol <bielpujadassuriol@gmail.com>
2024-02-14 23:10:16 +0100

    update

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ type = 'P' #S for sphere, C for cylinder, P for plane block (type must be char!)
 geometric_temp = [273.15+30,273.15+80]
 
 qv = 1E7 #internal generated heat.
-print(qv)
+# print(qv)
 
-lambda_transport = [300,300] #Deffine the number of lambdas from closest to x = 0 (or r = 0) to furthest (one for each iteration).
+lambda_transport = [0.5] #Deffine the number of lambdas from closest to x = 0 (or r = 0) to furthest (one for each iteration).
 alfa_convex = [30,200] #Deffine the number of convexion coeff from closest to x = 0 (or r = 0) to furthest.
 start_run = True
 Position = 0;
@@ -60,29 +60,51 @@ class Plane_block:
 
         tw2_sol = sp.solve((q_cb-q_b),tw2)
 
-        sp.pprint(tw2_sol)
+        # sp.pprint(tw2_sol)
 
         tw1_sol = tw1_sol[0].subs(tw2,tw2_sol[0])
-        print(c1)
+        # print(c1)
         c1 = c1.subs([ (tw2,tw2_sol[0]), (tw1, tw1_sol)])
         c2 = tw1_sol-273.15
-        print(c1)
+        # print(c1)
         self.T = self.T.subs([(q_v,self.qv), (lam,self.Lambda), (C1,c1), (C2,c2)])
         self.q_x = self.q_x.subs([(q_v, self.qv), (lam, self.Lambda), (C1, c1)])
         return self.T, self.q_x
     def Plot_T_and_q_x (self,step):
         x = sp.symbols('x')
+        pos_Temp_max = 0
+        first = True
         num_positions = int((self.Geometry[2])/step + 1)
         x_ = np.linspace(0,self.Geometry[2],num_positions)
         Temp = np.zeros(num_positions)
         q_x = np.zeros(num_positions)
+        cache = 0;
         for i in range(0,num_positions):
             Temp[i] = self.T.subs(x,x_[i])
             q_x[i] = self.q_x.subs(x,x_[i])
+            if cache != 0 and Temp[i-1] > Temp[i] and first == True:
+                pos_Temp_max = i-1
+                first = False
+            cache = i
 
 
-        plt.plot(x_,Temp)
-        plt.plot(x_,q_x)
+        print('Tmax is ' + str(Temp[pos_Temp_max]) + ' cº and its found in position ' + str(x_[pos_Temp_max]*1E3) + ' mm')
+        print('T at x=0 is T_0= ' + str(Temp[0]) + ' cº and T at x= ' + str(x_[cache]*1E3) + 'mm is T_e = ' + str(Temp[cache]) + 'ºc')
+        print('q at x=0 is q_0=' + str(q_x[0]) + ' w/m2 and q at x= ' + str(x_[cache]*1E3) + 'mm is q_e = ' + str(q_x[cache]) + 'w/m2')
+        plt.figure()
+        plt.title("Temperature in function of material lenght")
+        plt.plot(x_*1E3,Temp,linewidth=2, markersize=12,color = 'red')
+        plt.xlabel("Material lenght [mm]")
+        plt.ylabel("Temperature [cº]")
+        plt.grid()
+        plt.figure()
+        plt.title("Heat transfer in function of material lenght")
+        plt.plot(x_*1E3,q_x)
+        plt.xlabel("Material lenght [mm]")
+        plt.ylabel("Heat [w/m2]")
+        plt.grid()
+        plt.show()
+
 
 
 
@@ -116,7 +138,7 @@ match type:
     case 'P':
         Body = Plane_block (geometric_par,geometric_temp,lambda_transport,alfa_convex,qv,Position)
         [T,q_x] = Body.Solve_for_unique_body()
-        sp.pprint(T)
+        # sp.pprint(T)
         Body.Plot_T_and_q_x(1E-4)
 
 
