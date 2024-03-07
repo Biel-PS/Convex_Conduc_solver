28084a7a10b00c8a65dfd0f901ef1dc2cd08f53e
Biel Pujadas Suriol <bielpujadassuriol@gmail.com>
2024-03-07 17:43:40 +0100

    update

--- Aplication_cpp/Main.cpp
+++ Aplication_cpp/Main.cpp
@@ -7,24 +7,26 @@ using namespace std;
 //Declaration of global variables
 
 //FISICAL PARAMETERS
-double Rint = 0; // Internal radius of the fin
-double Rext = 10; // External radius of the fin
-double ef = 10E-3; // Fin's vertical length [m]
-double eb = 0; // Distance between fins [m]
-double Text = 10; // Temp at exterior [Cº]
-double T_wall = 100; // Temp of the wall r = Rint [Cº]
+const double Rint = 15E-3; // Internal radius of the fin
+const double Rext = 75E-3; // External radius of the fin
+const double ef = 14E-3; // Fin's vertical length [m]
+const double eb = 0; // Distance between fins [m]
+const double Text = 30; // Temp at exterior [Cº]
+const double T_wall = 250; // Temp of the wall r = Rint [Cº]
 //double lambda_f[2] = [200,10]; // lambda of the material (homogeneous case) [a_nT^n,...a_1T,a_0] (function of T)
-double lambda_f = 100;
-double alfa_exterior = 0; //alpha in the length of the fin with outter fluid
-double alfa_extrem = 0; //alpha in the end of fin
-double nf = 5; //Number of fins
+const double lambda_f = 386;
+const double alfa_exterior = 433; //alpha in the length of the fin with outter fluid
+const double alfa_extrem = 0; //alpha in the end of fin
+const double nf = 21; //Number of fins
 //NUMRICAL PARAMETERS
-const int N = 5; //Number of control volumes
-double delta = 1E-6; //Convergence criteria
-double Tstart = 30; //Initial temp map (supposed equal in all geometry) [Cº]
+const int N = 1000; //Number of control volumes
+const double delta = 1E-10; //Convergence criteria
+const double Tstart = 50; //Initial temp map (supposed equal in all geometry) [Cº]
+
 
 // DEFFINE VECTOR LENGTH AND OPERATION VARIABLES
 double T[N+2] = {0};
+double T_n[N+2] = {0};
 double rw[N+1] = {0}; //fronteres entre volumns de control i extrems
 double rp[N+2] = {0};
 double a_E[N+2] = {0};
@@ -33,6 +35,7 @@ double a_P[N+2] = {0};
 double b_P[N+2] = {0};
 double Ap[N] = {0};
 double S[N+2] = {0};
+
 
 double dpw = 0; // malla uniforme
 double dpe = 0;
@@ -62,7 +65,7 @@ class vec { //Vector deffinition class
         }
         for (int i = 0; i<N;i++){ //definició area superior i inferior dels volumns de control
             Ap[i] = 2 * M_PI * pow(rw[i+1],2)-pow(rw[i],2);
-            cout << Ap[i] << "\n";
+            //cout << Ap[i] << "\n";
         }
     }
 };
@@ -73,6 +76,7 @@ public:
     Mapa_inicial(){ //Definim el mapa inicial de temperatures
         for(int i = 0;i<(N+2);i++){
             T[i] = Tstart;
+            T_n[i] = T[i] + 10; // Ens assegurem que per la primera iteració no es compleix el criteri d'acceptació
         }
     }
 };
@@ -105,10 +109,40 @@ class Solver { //Initial temp map deffinition class
 public:
 
     void gauss_seidel(){ //Definim el mapa inicial de temperatures
+        int cont = 0;
+        while (true){
+            if(norm(T_n,T,delta)){
+                //cout << cont << "\n";
+                break;
+            }
+            else{
+                for (int i = 0; i<(N+2); i++){
+                    T[i] = T_n[i];
+                }
+                T_n[0] = (a_E[0]*T[1] + b_P [0])/a_P[0];
+                for (int i = 1; i<N+1;i++){
+                    T_n[i] = (a_E[i]*T[i+1] + a_W[i]*T[i-1] + b_P [i])/a_P[i];
+                }
+                T_n[N+1] = (a_W[N+1]*T[N] + b_P [N+1])/a_P[N+1];
+            }
+        cont++;}
 
-        }
+
     }
+
     void TDMA (){ }
+    void Qfin (){ }
+    void Qbody (){}
+    static bool norm (double T_1[N+2],double T_2[N+2],double delta){
+        bool done = true;
+        for (int i = 0; i< N+2;i++){
+             if (abs(T_1[i]-T_2[i]) > delta){
+                 done = false;
+                 break;
+             }
+        }
+        return sqrt(done);
+    }
 };
 
 
@@ -119,7 +153,10 @@ int main() {
     Mapa_inicial map;
     Coefs_discrtitzacio coef;
     Solver solver;
-
+    solver.gauss_seidel();
+    for (int i = 0; i<(N+2); i++){
+        cout << T[i] << "\n";
+    }
 
     return 0;
 }
