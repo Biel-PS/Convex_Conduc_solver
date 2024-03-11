e69e6a7a55f79a78b9f16b61d16c10dd152e89eb
Biel Pujadas Suriol <bielpujadassuriol@gmail.com>
2024-03-11 18:08:57 +0100

    update

--- Aplication_cpp/Main.cpp
+++ Aplication_cpp/Main.cpp
@@ -8,19 +8,19 @@ using namespace std;
 //Declaration of global variables
 
 //FISICAL PARAMETERS
-const double Rint = 15E-3; // Internal radius of the fin
-const double Rext = 75E-3; // External radius of the fin
+const double Rint = 1; // Internal radius of the fin
+const double Rext = 3; // External radius of the fin
 const double ef = 4E-3; // Fin's vertical length [m]
 const double eb = 5E-3; // Distance between fins [m]
 const double Text = 25; // Temp at exterior [Cº]
-const double T_wall = 72.13; // Temp of the wall r = Rint [Cº]
+const double T_wall = 200; // Temp of the wall r = Rint [Cº]
 //double lambda_f[2] = [200,10]; // lambda of the material (homogeneous case) [a_nT^n,...a_1T,a_0] (function of T)
-const double lambda_f = 58;
-const double alfa_exterior = 110; //alpha in the length of the fin with outter fluid
-const double alfa_extrem = 0; //alpha in the end of fin
+const double lambda_f = 100;
+const double alfa_exterior = 200; //alpha in the length of the fin with outter fluid
+const double alfa_extrem = 50; //alpha in the end of fin
 const double nf = 163; //Number of fins
 //NUMRICAL PARAMETERS
-const int N = 300; //Number of control volumes
+const int N = 500; //Number of control volumes
 const double delta = 1E-5; //Convergence criteria
 const double Tstart = 100; //Initial temp map (supposed equal in all geometry) [Cº]
 
@@ -35,7 +35,7 @@ double a_W[N+2] = {0};
 double a_P[N+2] = {0};
 double b_P[N+2] = {0};
 double Ap[N] = {0};
-double S[N+2] = {0};
+double S[N+1] = {0};
 
 
 double dpw = 0; // malla uniforme
@@ -49,7 +49,7 @@ double Q_base_fin = 0;
 
 
 class vec { //Vector deffinition class
-    public:
+public:
 
     vec(){
         deltaR = (Rext-Rint)/N;
@@ -58,18 +58,24 @@ class vec { //Vector deffinition class
 
         for (int i = 0; i<N+1;i++){ //Definició de el vector r_w
             rw[i] = Rint + (i)*deltaR;
-            //cout << rw[i] << "\n";
+           // cout << rw[i] << "\n";
         }
-        rp[0] = Rint; //Definició components inicials del vector r_w
-        rp[N+1] = Rext;
+        rp[0] = rw[0] ;
+        rp[N+1] = rw[N];
+        for (int i = 1; i<N+1;i++){ //Definició de el vector r_w
+            rp[i] = (rw[i] + rw[i-1])/2;
+        }
+       /* for(double & i : rp){
+            cout << i << "\n";
+        }*/
 
         for (int i = 0; i<N+1;i++){ // definició de les arees laterals (parets entre Volumns de control)
             S[i] = rw[i] * 2 *M_PI * ef ;
-            //cout << S[i] << "\n";
+          // cout << S[i] << "\n";
         }
         for (int i = 0; i<N;i++){ //definició area superior i inferior dels volumns de control
-            Ap[i] = 2 * M_PI * (pow(rw[i+1],2)-pow(rw[i],2));
-            //cout << Ap[i] << "\n";
+            Ap[i] = 2 * M_PI * (pow(rw[i+1],2.0)-pow(rw[i],2.0));
+           // cout << Ap[i] << "\n";
         }
     }
 };
@@ -78,9 +84,10 @@ class Mapa_inicial { //Initial temp map deffinition class
 public:
 
     Mapa_inicial(){ //Definim el mapa inicial de temperatures
-        for(int i = 0;i<(N+2);i++){
-            T[i] = Tstart;
-            // T_n[i] = T[i] + 10;  Ens assegurem que per la primera iteració no es compleix el criteri d'acceptació
+        for(double & i : T){
+            i = Tstart;
+            //cout << i <<"\n";
+          //  T_n[i] = T[i] + 10; // Ens assegurem que per la primera iteració no es compleix el criteri d'acceptació
         }
     }
 };
@@ -124,37 +131,39 @@ public:
                     T[i] = T_n[i];
                 }
                 T_n[0] = (a_E[0]*T[1] + b_P [0])/a_P[0];
-                for (int i = 1; i<N+1;i++){
-                    T_n[i] = (a_E[i]*T[i+1] + a_W[i]*T[i-1] + b_P [i])/a_P[i];
+                T_n[N+1] = (a_W[N+1]*T[N] + b_P[N+1])/a_P[N+1];
+                for (int i = 1; i< N+1; i++){
+                    T_n[i] = (a_E[i]*T[i+1] + a_W[i]*T[i-1] + b_P[i])/a_P[i];
                 }
-                T_n[N+1] = (a_W[N+1]*T[N] + b_P [N+1])/a_P[N+1];
             }
-        cont++;}
-
-
+            cont++;
+        }
     }
 
     void TDMA (){ }
     static void Qfin (){
         Q_f = 0;
+
         for (int i = 1 ; i<N+1;i++){
             Q_f += alfa_exterior*(T_n[i]-Text)*Ap[i-1];
         }
-        Q_f += alfa_extrem*(T_n[N+1] - Text)*2*M_PI *Rext * ef;
-       // Q_f *= nf;
-        Q_base_fin  = - lambda_f* ((T_n[1]-T_n[0])/dpe) * 2 * M_PI * Rint * ef;
+        Q_f += alfa_extrem * (T_n[N+1] - Text) * 2 * M_PI * Rext * ef;
+        // Q_f *= nf;
+        Q_base_fin  = - lambda_f*((T_n[1]-T_n[0])/dpe) * (2 * M_PI * Rint * ef);
 
     }
     static void Qbody (){
-        Q_b  = alfa_exterior*(T_wall - Text)*2 * M_PI * Rint * eb;
+        Q_b  = alfa_exterior*(T_wall - Text)* 2 * M_PI * Rint * eb;
     }
     static bool norma (double T_1[N+2],double T_2[N+2]){
         bool done = true;
+        double T_cache = 0;
         for (int i = 0; i< N+2;i++){
-             if (norm(T_1[i]-T_2[i]) >= delta){
-                 done = false;
-                 break;
-             }
+            T_cache +=pow((T_1[i]-T_2[i]),2.0);
+            }
+        cout << sqrt(T_cache) << "\n";
+        if (sqrt(T_cache) >= delta){
+            done = false;
         }
         return done;
     }
@@ -171,13 +180,13 @@ int main() {
     Solver::gauss_seidel();
 
     for (double i : T){
-        cout << i << "\n";
+     // cout << i << "\n";
     }
 
     Solver::Qfin();
     Solver::Qbody();
 
-    cout <<" Q_fin: " << Q_f <<"\n Q_body: "<<Q_b <<"\n Q_base_fin: "<<Q_base_fin << "\n Error factor: "<<(abs(Q_f - Q_base_fin)) ;
+    cout <<" Q_fin: " << Q_f <<"\n Q_body: "<<Q_b <<"\n Q_base_fin: "<<Q_base_fin << "\n Error : "<<abs(Q_base_fin - Q_f) ;
 
 
     return 0;
